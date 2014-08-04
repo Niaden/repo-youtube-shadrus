9c083e544ff45a7d051f79ff76145c989ddfd9c7
Damir Ravilov <damir_ne_@mail.ru>
2014-08-04 14:57:22 +0300

    end of 8th lesson https://www.youtube.com/watch?v=V7IwdSJRe9I

--- article/views.py
+++ article/views.py
@@ -1,10 +1,13 @@
 # coding: utf8
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, Http404
 from django.template.loader import get_template
 from django.template import Context
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from article.models import Article, Comments
+from django.core.exceptions import ObjectDoesNotExist
+from .forms	import CommentForm
+from django.core.context_processors import csrf
 
 # Create your views here.
 
@@ -28,4 +31,28 @@ def articles(request):
 
 def article(request, article_id=1):
 	#objects.get - юзать только, когда 1 объект(в нашем случае, запись) возвращаешь(если признаку в get будут удовлетворять более, чем 1 объект, будет ошибка). objects.filter - то же самое, только возвращается несколько объектов по признаку возвращаешь.
-	return render_to_response('article.html', {'article': Article.objects.get(id=article_id), 'comments': Comments.objects.filter(comments_article_id=article_id)})
+	comment_form = CommentForm
+	args = {}
+	args.update(csrf(request))
+	args['article'] = Article.objects.get(id=article_id)
+	args['comments'] = Comments.objects.filter(comments_article_id=article_id)
+	args['form']=comment_form
+	return render_to_response('article.html',args)
+
+def addlike(request, article_id):
+	try:
+		article = Article.objects.get(id=article_id)
+		article.article_likes += 1
+		article.save()
+	except ObjectDoesNotExist:
+		raise Http404
+	return redirect("/")	
+
+def addcomment(request, article_id):
+	if request.POST:
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			comment = form.save(commit=False)
+			comment.comments_article = Article.objects.get(id=article_id)
+			form.save()
+	return redirect('/articles/get/%s/' % article_id)
